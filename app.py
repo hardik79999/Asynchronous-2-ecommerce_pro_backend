f797c1bcd238a6dc170db4f0f71d6ac544c62354
hardik79999 <hardikbandhiya2004@gmail.com>
2026-04-07 14:18:23 +0530

    fixx

--- app.py
+++ app.py
@@ -42,7 +42,4 @@ def create_app(config_class=Config):
 
 if __name__ == '__main__':
     app = create_app()
-    host = os.environ.get('FLASK_HOST', '0.0.0.0')
-    port = int(os.environ.get('FLASK_PORT', 8000))
-    debug = os.environ.get('FLASK_DEBUG', 'True').lower() == 'true'
-    app.run(host=host, port=port, debug=debug)
+    app.run(port=5000, debug=True)
